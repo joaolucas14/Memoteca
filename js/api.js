0a01b0834b48c80811ba7f78f315c2f3a4d51561
joaolucas14 <jlparceiro10@gmail.com>
2024-09-06 16:53:09 -0300

    criando as requisiçoes na API de busca e edição de um pensamento

--- js/api.js
+++ js/api.js
@@ -25,7 +25,33 @@ const api = {
             alert('Erro ao buscar pensamentos')
             throw error
         }
-    }
+    },
+
+    async buscarPensamentoPorId(id){
+        try {
+            const response = await fetch(`http://localhost:3000/pensamentos/${id}`)
+            return await response.json()
+        } catch {
+            alert("Erro ao buscar Pensamento")
+            throw error
+        }
+    },
+    async editarPensamento(pensamento){
+        try{
+            const response = await fetch(`http://localhost:3000/pensamentos/${pensamento.id}`, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(pensamento)
+            })
+            return await response.json()
+        }
+        catch(error){
+            alert('Erro ao editar pensamentos')
+            throw error
+        }
+    },
 }
 
 export default api;
